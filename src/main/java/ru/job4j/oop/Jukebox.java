c580f6db91c1fd184144fe9077e9e9875337fefe
User <funkystore.krd@gmail.com>
2023-11-14 10:21:32 +0300

    1.3. Вызов метода с аргументами. [#166086 #459571]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -2,18 +2,23 @@ package ru.job4j.oop;
 
 public class Jukebox {
     public void music(int position) {
-        if (position == 1) {
-            System.out.println("Пусть бегут неуклюже");
-        } else if (position == 2) {
-            System.out.println("Спокойной ночи");
-        } else {
-            System.out.println("Песня не найдена");
-        }
     }
 
     public static void main(String[] args) {
         Jukebox vladimir = new Jukebox();
         int position = 3;
-        vladimir.music(position);
+        switch (position) {
+            case 1:
+                System.out.println("Пусть бегут неуклюже");
+                break;
+            case 2:
+                System.out.println("Спокойной ночи");
+                break;
+            case 3:
+                System.out.println("Песня не найдена");
+                break;
+            default:
+                System.out.println("Ни одно из условий не подошло");
+        }
     }
 }
